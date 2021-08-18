8a0bd45fbf4fe490a1ce402c2861ba1ccd1284de
zhangzz2015 <84154094+zhangzz2015@users.noreply.github.com>
2021-08-17 21:00:26 -0700

    Update remove_element.cpp

--- array/remove_element.cpp
+++ array/remove_element.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int totalNum=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[totalNum] = nums[i];
+                totalNum++;
+            }        
+        }
+        return totalNum;        
+    }
+};
