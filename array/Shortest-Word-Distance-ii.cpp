8ec01decbf1cab5b1cda0e2f73692793b3111f78
zhangzz2015 <84154094+zhangzz2015@users.noreply.github.com>
2021-08-17 23:26:34 -0700

    Update Shortest-Word-Distance-ii.cpp

--- array/Shortest-Word-Distance-ii.cpp
+++ array/Shortest-Word-Distance-ii.cpp
@@ -1,1 +1,31 @@
+class WordDistance {
+public:
+    unordered_map<string, vector<int>> recordWords;
+    int wordsDictSize =0; 
+    WordDistance(vector<string>& wordsDict) {
+        
+        for(int i=0; i< wordsDict.size(); i++)
+        {
+            recordWords[wordsDict[i]].push_back(i);
+        }
+        wordsDictSize = wordsDict.size();
+    }
+    
+    int shortest(string word1, string word2) {
+        int ret = wordsDictSize; 
+        for(int i=0; i< recordWords[word1].size(); i++)
+        {
+            for(int j=0; j< recordWords[word2].size(); j++)
+            {
+                ret = min(ret, abs(recordWords[word1][i] - recordWords[word2][j]));
+            }
+        }
+        return ret; 
+    }
+};
 
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance* obj = new WordDistance(wordsDict);
+ * int param_1 = obj->shortest(word1,word2);
+ */
