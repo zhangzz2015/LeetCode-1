80e66f198137febcbe8ed284fa9a651fd493088e
zhangzz2015 <84154094+zhangzz2015@users.noreply.github.com>
2021-08-17 22:10:08 -0700

    Update Find_the_celebrity.cpp

--- array/Find_the_celebrity.cpp
+++ array/Find_the_celebrity.cpp
@@ -1,47 +1,28 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
 class Solution {
 public:
     int findCelebrity(int n) {
         
-        /// use two pointer. 
-        int left =0; 
-        int right = n-1; 
-        while(left<right)
+      ///  find candiate first. 
+        int candidate =0; 
+        for(int i=0; i< n ; i++)
         {
-            bool bleft = knows(left, right);
-            bool bright = knows(right, left); 
-            if(bleft && bright)//  left know right. it means that left must not Celebrity person. 
+            if(knows(candidate, i))
             {
-                left++;
-                right--; 
-            }
-            else if(bleft)
-            {
-                left++; 
-            }
-            else if(bright) // right know left. it means that righ must not celeberity person. 
-            {
-                right--;
-            }
-            else  // left and right both are not celerity person. 
-            {
-                left++; 
-                right--; 
+                candidate = i; 
             }
         }
-
-        //cout << "left:" << left <<"right:" << right << "\n"; 
-        if(left == right)
+         // verify if candiate is real or not
+        for(int i =0; i< n ; i++)
         {
-            for(int i =0; i< n ; i++)
-            {
-                if(i == left)
-                    continue;
-                if(knows(i, left) ==false || knows(left, i)==true )
-                    return -1;
-            }
-             return left;  
-        } 
-        else
-            return -1; 
+            if(i == candidate)
+                continue;
+            if(knows(i, candidate) ==false || knows(candidate, i)==true )
+                return -1;
+        }
+        return candidate;  
+
     }
 };
